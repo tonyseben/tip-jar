3f67281e1947fb68be22022bcb38b415222c7c00
tonyseben <tonysbento@gmail.com>
2024-05-17 17:38:24 +0530

    load image in history list

--- app/src/main/java/com/example/tipjar/ui/history/component/HistoryItem.kt
+++ app/src/main/java/com/example/tipjar/ui/history/component/HistoryItem.kt
@@ -1,20 +1,23 @@
 package com.example.tipjar.ui.history.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.tipjar.R
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.tipjar.ui.home.HistoryItemUiState
 
 @Composable
@@ -46,13 +49,24 @@ fun HistoryItem(history: HistoryItemUiState) {
             fontSize = 13.sp,
             fontWeight = FontWeight.Bold
         )
-        Image(
-            painter = painterResource(id = R.drawable.ic_history),
-            contentDescription = "",
+
+        Box(
             modifier = Modifier
                 .padding(start = 8.dp)
                 .size(52.dp)
                 .align(Alignment.CenterEnd)
-        )
+                .clip(RoundedCornerShape(8.dp))
+        ) {
+            history.receiptUri?.let { receiptUri ->
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(receiptUri)
+                        .build(),
+                    contentDescription = "Receipt",
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+
     }
 }
