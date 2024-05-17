4cba516ba268289b733c9f11cb96a6e565b8f55d
tonyseben <tonysbento@gmail.com>
2024-05-18 01:57:17 +0530

    launcher icon updated

--- app/src/main/java/com/example/tipjar/MainActivity.kt
+++ app/src/main/java/com/example/tipjar/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tipjar
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -24,6 +25,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
         enableEdgeToEdge()
         setContent {
             TipJarTheme {
